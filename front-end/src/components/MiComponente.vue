e62cbada1fd7ce2edc1843e378fd4777eb27693f
JesusA2004 <arizma2004@gmail.com>
2025-04-10 22:08:10 -0600

    Cambios front

---
+++ front-end/src/components/MiComponente.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="mi-componente">
+        <h1>{{ mensaje }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'mi-componente',
+        data() {
+            return {
+                mensaje: 'Hola, este es mi componente Vue!'
+            };
+        }
+    };
+</script>
