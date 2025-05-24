47b360e025809b2f42e398426a7dab17fdb24db2
JesusA2004 <arizma2004@gmail.com>
2025-05-24 16:28:10 -0600

    Cambio users

--- Back-End/app/Models/User.php
+++ Back-End/app/Models/User.php
@@ -16,6 +16,7 @@ class User extends Authenticatable implements JWTSubject
         'password',
         'email',
         'rol',
+        'estatus',
         'urlFotoPerfil',
         'persona_id',
     ];
